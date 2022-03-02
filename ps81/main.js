acff2c301cca7a32a965cd012f511283c7da53a3
Rishab Agarwala <ragarwala2097@gmail.com>
2022-03-02 17:54:24 +0530

    updated ps81/main.js

--- ps81/main.js
+++ ps81/main.js
@@ -1,13 +1,13 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
-
+ 
 let inputString = "";
 let currentLine = 0;
-
+ 
 process.stdin.on("data", (inputStdin) => {
   inputString += inputStdin;
 });
-
+ 
 process.stdin.on("end", (_) => {
   inputString = inputString
     .trim()
@@ -17,32 +17,32 @@ process.stdin.on("end", (_) => {
     });
     main();
 });
-
+ 
 function readLine() {
   return inputString[currentLine++];
 }
-
-
+ 
+ 
 function solve(){
     let inp = readLine().split(" ").map((x) => parseInt(x));
     let x = parseInt(inp[0],10);
     let k = parseInt(inp[1],10);
     let d = parseInt(inp[2],10);
-    X = Math.abs(x);
-    if(x/d>k)
+    x = Math.abs(x);
+    if(Math.floor(x/d)>k)
         console.log(x-k*d);
     else{
         let y = x%d;
-        let tt = x/d;
+        let tt = Math.floor(x/d);
         if((k-tt)%2==1)
             y = Math.abs(d-y);
         console.log(y);
     }
 }
-
-
-
-
+ 
+ 
+ 
+ 
 function main(){
     let inp = readLine().split(" ").map((x) => parseInt(x));
     let t = inp[0];
